ba695dcc71caa5c99e4e58688878fcf4c5d9d4aa
Jesus-QC <jesusquirocampos@gmail.com>
2022-07-22 16:10:03 +0200

    Initial commit

---
+++ QueueSystem/QueueSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Player = Exiled.Events.Handlers.Player;
+
+namespace QueueSystem
+{
+    public class QueueSystem : Plugin<PluginConfig>
+    {
+        public override string Author { get; } = "Jesus-QC";
+        public override string Name { get; } = "QueueSystem";
+        public override string Prefix { get; } = "queue_system";
+        public override Version Version { get; } = new (1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new (5, 2, 2);
+
+        public override void OnEnabled()
+        {
+            Player.PreAuthenticating += OnPreAuthenticating;
+            
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Player.PreAuthenticating -= OnPreAuthenticating;
+            
+            base.OnDisabled();
+        }
+
+        private void OnPreAuthenticating(PreAuthenticatingEventArgs ev)
+        {
+            Log.Info(ev.UserId + " >> a");
+            Log.Info(ev.ServerFull + " " + !ReservedSlot.HasReservedSlot(ev.UserId));
+            
+            if(ev.ServerFull && !ReservedSlot.HasReservedSlot(ev.UserId))
+                ev.Delay(4, true);
+        }
+    }
+}
