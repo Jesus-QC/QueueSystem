7226f54e2fa081c550d58009e24840dbc7c3eff0
Jesus-QC <jesusquirocampos@gmail.com>
2022-10-31 22:21:26 +0100

    1.0.1

--- QueueSystem/PluginConfig.cs
+++ QueueSystem/PluginConfig.cs
@@ -11,6 +11,6 @@ namespace QueueSystem
         public byte Delay { get; set; } = 4;
 
         [Description("The first attempt to join of the player will be received with a message.")]
-        public string AlertMessage { get; set; } = "Join again to be added to a automatic queue.\nYou will automatically join the server once there is a slot available!";
+        public string AlertMessage { get; set; } = "<color=red>Join again to be added to a automatic queue.</color>\nYou will automatically join the server once there is a slot available!";
     }
 }
